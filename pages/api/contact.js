a6c3a971619ccbf9cd5e7a9fc0d2a8bacc91a1f8
Dylan Tran <dtran2386@yahoo.com>
2022-07-08 16:28:16 -0400

    add promises to mailer functionality

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,9 +1,12 @@
+import { resolveHref } from "next/dist/next-server/lib/router/router";
+
 let nodemailer = require("nodemailer");
 
 const password = process.env.NEXT_PUBLIC_GMAIL_APP_PASSWORD;
 
-export default function (req, res) {
+export default async function (req, res) {
   console.log("body", req.body);
+
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
@@ -14,20 +17,44 @@ export default function (req, res) {
     secure: true,
   });
 
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
   const mailData = {
-    from: "trylandan@gmail.com",
+    from: {
+      name: req.body.name,
+      address: "trylandan@gmail.com",
+    },
     to: "dtranrules@gmail.com",
-    subject: `Message From ${req.body.name}`,
+    subject: `Message From ${req.body.name} via portfolio website`,
     text: req.body.message,
     html: `<div>${req.body.message}</div>`,
   };
 
-  transporter.sendMail(mailData, function (err, info) {
-    if (err) console.log(err);
-    else console.log(info);
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
   });
 
-  res.status(200).send({
-    message: "Email successfully sent.",
+  res.status(200).json({
+    status: "OK",
   });
 }
