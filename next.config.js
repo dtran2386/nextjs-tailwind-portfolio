845639e84671085d2eb842a5c8df89726d4ff9ee
Dylan Tran <dtran2386@yahoo.com>
2022-07-08 16:05:06 -0400

    add env var to next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_GMAIL_APP_PASSWORD: process.env.NEXT_PUBLIC_GMAIL_APP_PASSWORD,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
